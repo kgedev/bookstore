acabf7868bf6c5908998986b4344846b4f16aaa7
sv <svarog96@gamil.com>
2020-02-24 16:03:12 +0500

    bs: add bookstore configuration

---
+++ src/main/java/ru/gkdev/bookstore/config/WebConfig.java
@@ -0,0 +1,13 @@
+package ru.gkdev.bookstore.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"ru.gkdev.bookstore.controller"})
+public class WebConfig implements WebMvcConfigurer {
+
+}
